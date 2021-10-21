6d1acfeeb3b43a804fbbe6cfd52e5b0c97103673
unknown <arifypp@gmail.com>
2021-10-21 16:18:17 +0600

    Teacher All Screen Update Done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,3 +57,91 @@ $(document).ready(function() {
     });
   
   });
+
+
+  $(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultDate: '2016-09-12',
+        navLinks: true, // can click day/week names to navigate views
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end) {
+            // Display the modal.
+            // You could fill in the start and end fields based on the parameters
+            $('.modal').modal('show');
+
+        },
+        eventClick: function(event, element) {
+            // Display the modal and set the values to the event values.
+            $('.modal').modal('show');
+            $('.modal').find('#title').val(event.title);
+            $('.modal').find('#starts-at').val(event.start);
+            $('.modal').find('#ends-at').val(event.end);
+
+        },
+        editable: true,
+        eventLimit: true // allow "more" link when too many events
+
+    });
+
+
+
+    // Whenever the user clicks on the "save" button om the dialog
+    $('#save-event').on('click', function() {
+        var title = $('#title').val();
+        if (title) {
+            var eventData = {
+                title: title,
+                start: $('#starts-at').val(),
+                end: $('#ends-at').val()
+            };
+            $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+        }
+        $('#calendar').fullCalendar('unselect');
+
+        // Clear modal inputs
+        $('.modal').find('input').val('');
+
+        // hide modal
+        $('.modal').modal('hide');
+    });
+
+
+
+
+
+    $(document).ready(function() {
+			var my_calendar = $("#calendar").fullCalendar({
+                dataTitles: { defaultDate: 'default', today : 'Today' },
+                notes: [
+                		{ "date": "2016-02-25", "note": ["date here"] },
+                		{ "date": "2016-02-28", "note": ["date here"] }
+                		],
+                showNotes: true,
+                dayClick: function(date, view) {
+                var selecteddate = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+                	$('#myModal').modal('show').on('shown.bs.modal', function () {
+                    $('.modal-body').html(selecteddate);
+			    });
+                }
+			});
+
+			// init calendar
+			my_calendar.build();
+
+			// update calendar
+			my_calendar.update({
+				minDate: "2016-02-23",
+				defaultDate: new Date()
+			});
+		});
+
+
+    
+});
